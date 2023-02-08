43bdab18a2a1fe38aa2f3f2ba3ac4e8027516927
Sanjay <ksanjayk@protonmail.com>
2023-02-09 00:10:35 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,106 @@
-import React from 'react'
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import React, { useState } from 'react';
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
+import { Mail, Notifications } from '@mui/icons-material';
 
+const StyledToolbar = styled(Toolbar)({
+  display: 'flex',
+  justifyContent: 'space-between',
+});
+const Search = styled('div')(({ theme }) => ({
+  backgroundColor: 'white',
+  pading: '0 10px',
+  borderRadius: theme.shape.borderRadius,
+  width: '40%',
+}));
+const Icons = styled('Box')(({ theme }) => ({
+  //   backgroundColor: 'white',
+  display: 'none',
+  alignItems: 'center',
+  gap: '20px',
+  [theme.breakpoints.up('sm')]: {
+    display: 'flex',
+  },
+}));
+const UserBox = styled('Box')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+  [theme.breakpoints.up('sm')]: {
+    display: 'none',
+  },
+}));
 function Navbar() {
+  const [open, setOpen] = useState(false);
+  const handleClose = (e) => {
+    console.log('handleClose');
+  };
   return (
-    <div>Navbar</div>
-  )
+    // position='stick' will make navbar on top
+    // even when we scroll down
+    <AppBar position='stick'>
+      <StyledToolbar>
+        <Typography variant='h6' sx={{ display: { xs: 'none', sm: 'block' } }}>
+          Book Club
+        </Typography>
+        <LocalLibraryIcon sx={{ display: { xs: 'block', sm: 'none' } }} />
+        <Search>
+          <InputBase placeholder='search' />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color='error'>
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color='error'>
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            alt='Remy Sharp'
+            src=''
+            onClick={(e) => setOpen(true)}
+          />
+        </Icons>
+        <UserBox>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            alt='Remy Sharp'
+            src=''
+            onClick={(e) => setOpen(true)}
+          />
+          <Typography variant='span'>Sanji</Typography>
+        </UserBox>
+      </StyledToolbar>
+      <Menu
+        id='demo-positioned-menu'
+        aria-labelledby='demo-positioned-button'
+        open={open}
+        onClose={e => setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  );
 }
 
-export default Navbar
+export default Navbar;
