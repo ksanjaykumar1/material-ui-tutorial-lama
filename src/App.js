6c81cbde1e00ed94896d27078ab73b8cc8c327d1
Sanjay <ksanjayk@protonmail.com>
2023-02-09 00:10:11 +0530

    basic components setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Feed from './components/Feed';
+import Navbar from './components/Navbar';
+import Rightbar from './components/Rightbar';
+import Sidebar from './components/Sidebar';
+
 function App() {
   return (
-    <div className='App'>
-     Hello World
+    <div >
+      <Navbar/>
+      <Sidebar />
+      <Feed />
+      <Rightbar />
     </div>
   );
 }
